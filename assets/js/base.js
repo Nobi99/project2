1de0539f3cd87a86d411b8717d8a72159128399b
Nobi99 <chien.tt172978@sis.hust.edu.vn>
2020-05-27 09:26:11 +0700

    finishing category

---
+++ assets/js/base.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     "use strict"
+//     // var midCart1 =  document.querySelector(".mid-cart");
+//     // var cartDetail = document.querySelector(".mid-cart__detail");
+//     // midCart1.addEventListener("mouseover", function() {
+//     //     cartDetail.classList.add("visible");
+//     // });
+
+//     // midCart1.addEventListener("mouseout", function() {
+//     //     cartDetail.classList.remove("visible");
+//     // });
+
+    
+
+// }, false);
+
